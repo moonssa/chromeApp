3ba2151273d31d14435a73698cc8a9bf2f93d3ad
moonk <mkjoehsb@gmail.com>
2023-07-20 09:04:01 +0900

    save login-id with localStorage

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 const loginForm = document.getElementById("login-form");
 const loginInput = loginForm.querySelector("input");
 const greeting = document.querySelector("#greeting");
@@ -7,9 +8,21 @@ function handleLogin(event) {
   event.preventDefault();
 
   const username = loginInput.value;
+  localStorage.setItem(USERNAME_KEY, username);
   loginForm.classList.add(HIDDEN_CLASSNAME);
-  greeting.innerText = `Hello ${username}`;
-  greeting.classList.remove(HIDDEN_CLASSNAME);
+  paintGreetings(username);
 }
 
-loginForm.addEventListener("submit", handleLogin);
+function paintGreetings(username) {
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+  greeting.innerText = `Hello ${username}`;
+}
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+if (savedUsername === null) {
+  // show login form
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", handleLogin);
+} else {
+  // show headers
+  paintGreetings(savedUsername);
+}
